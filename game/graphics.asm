2b335b8a8992959dae96d48a45c6afd4c2da6e7c
Rachel Weil <partytimehexcellent@gmail.com>
2019-05-11 22:23:15 -0500

    Adds text for four gamestate screens.

---
+++ game/graphics.asm
@@ -0,0 +1,15 @@
+attract_txt:
+    .db $20, $82, %01100110
+    .db "ATTRACT"
+
+end_txt:
+    .db $20, $82, %01100010
+    .db "END"
+
+game_txt:
+    .db $20, $82, %01100011
+    .db "GAME"
+
+title_txt:
+    .db $20, $82, %01100100
+    .db "TITLE"
