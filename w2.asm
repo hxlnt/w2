4450864265fa48118f4f79d8288bf49b95b5fda9
Rachel Weil <partytimehexcellent@gmail.com>
2019-05-30 18:19:39 -0400

    Refactors w2 and library files.

--- w2.asm
+++ w2.asm
@@ -16,8 +16,8 @@
     .bank 0 ;;;;;;;;;;;;;;;;;;;;;;;;;  BANK 0: PROGRAM CODE
     .org $8000 ;;;;;;;;;;;;;;;;;;;;;;  $8000 - $9FFF
 
-    .include "lib/console_init.asm" ;  Import initialization
-    .include "game/init.asm"        ;    subroutines
+    .include "lib/console_init.asm" ;  Import and run initial-
+    .include "game/init.asm"        ;    ization subroutines
 
     JSR TurnScreenOff               ;  Turn screen off
     
