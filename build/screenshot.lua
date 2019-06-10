8b1b5ec7785055a000c670c70819f96f2839dc43
Rachel Weil <partytimehexcellent@gmail.com>
2019-06-10 18:49:12 -0500

    Changes on-screen text to "TEST" for testing purposes.

--- build/screenshot.lua
+++ build/screenshot.lua
@@ -1,12 +1,12 @@
-local date_table = os.date("*t")
-local hour, minute = date_table.hour, date_table.min
-local year, month, day = date_table.year, date_table.month, date_table.day
-local timestamp = string.format("%d %d, %d  %d:%d", month, day, year, hour, minute)
+-- local date_table = os.date("*t")
+-- local hour, minute = date_table.hour, date_table.min
+-- local year, month, day = date_table.year, date_table.month, date_table.day
+-- local timestamp = string.format("%d %d, %d  %d:%d", month, day, year, hour, minute)
 
 emu.frameadvance();
 emu.frameadvance();
 emu.frameadvance();
 emu.frameadvance();
 
-gui.text(16, 16, timestamp);
+gui.text(16, 16, "TEST");
 gui.savescreenshotas("screenshot.png");
